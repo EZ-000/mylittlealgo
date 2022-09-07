a6ee73bd97a5d1f587774b1b36090e84a522e098
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2022-09-07 19:46:17 +0900

    [Silver I] Title: 회의실 배정, Time: 4360 ms, Memory: 46496 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,17 @@
+N = int(input())
+meetings = []
+for _ in range(N):
+    S, E = map(int, input().split())
+    meetings += [[S, E]]
+
+meetings.sort(key=lambda x: x[0])
+meetings.sort(key=lambda x: x[1])
+
+cnt = 1
+E = meetings[0][1]
+for i in range(1, N):
+    if E <= meetings[i][0]:
+        cnt += 1
+        E = meetings[i][1]
+
+print(cnt)
