3facbac8a841362f664cff1325966e69ac9740ab
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2022-09-28 00:03:09 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 864 ms, Memory: 64864 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,19 +1,15 @@
 import sys
 input = sys.stdin.readline
-
+sys.setrecursionlimit(10 ** 6)
 
 # dfs 함수 정의
-def dfs(v, result):
-    global visited
-    visited += [v]
-    stack = [v]
-    while stack:
-        now = stack.pop()
-        for node in graph[now]:
-            if node not in visited:
-                visited += [node]
-                stack.append(node)
-    return
+def dfs(v, visited):
+    global result
+    visited[v] = 1
+    result.append(v)
+    for vertex in graph[v]:
+        if not visited[vertex]:
+            dfs(vertex, visited)
 
 
 # 연결 정보를 dict graph에 저장
@@ -31,12 +27,15 @@ for _ in range(M):
 # graph에 key가 남아있는 동안 dfs 탐색
 # dfs가 한 번 끝날 때마다 cnt 증가
 cnt = 0
-visited = []
-while len(visited) < N:
-    for idx in range(1, N + 1):
-        if idx not in visited:
-            start = idx
-            break
-    dfs(start, [])
+while graph.keys():
+    visited = [0] * (N + 1)
+    start = list(graph.keys())[0]
+    result = []
+    dfs(start, visited)
     cnt += 1
+
+    for v in result:
+        if v in graph.keys():
+            del graph[v]
+
 print(cnt)
