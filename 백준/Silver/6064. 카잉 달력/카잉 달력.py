5aee77c5d79d120692414fd28f906a30836bd7d0
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2022-09-28 10:14:16 +0900

    [Silver I] Title: 카잉 달력, Time: 3632 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+
+T = int(input())
+for _ in range(T):
+    M, N, x, y = map(int, input().split())
+    while True:
+        if M * N < x:
+            print(-1)
+            break
+        if x % N == y % N:
+            print(x)
+            break
+        x += M
