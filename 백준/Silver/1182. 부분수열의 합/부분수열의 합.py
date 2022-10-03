654ea2fd9ae850debd11726e417bfa19076eb2ff
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2022-10-03 22:07:13 +0900

    [Silver II] Title: 부분수열의 합, Time: 3784 ms, Memory: 222384 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,12 +1,31 @@
+# subset_recursion
+import sys
+input = sys.stdin.readline
+
+
+def subset_recur(i, k):
+    if i == k:
+        temp = []
+        for j in range(k):
+            if bit[j]:
+                temp += [seq[j]]
+        subset.append(temp)
+    else:
+        bit[i] = 0
+        subset_recur(i + 1, k)
+        bit[i] = 1
+        subset_recur(i + 1, k)
+
+
 N, S = map(int, input().split())
 seq = list(map(int, input().split()))
+bit = [0] * N
+subset = []
+
+subset_recur(0, N)
 
 cnt = 0
-for i in range(1 << N):
-    subset = []
-    for j in range(N):
-        if i & (1 << j):
-            subset.append(seq[j])
-    if subset and sum(subset) == S:
+for s in subset:
+    if s and sum(s) == S:
         cnt += 1
 print(cnt)
