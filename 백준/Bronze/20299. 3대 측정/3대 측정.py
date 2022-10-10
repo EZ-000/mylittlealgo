45d49565e1bbe22824dad2d281ad19ef59780694
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2022-10-11 00:05:50 +0900

    [Bronze II] Title: 3대 측정, Time: 548 ms, Memory: 196100 KB -BaekjoonHub

--- 백준/Bronze/20299. 3대 측정/3대 측정.py
+++ 백준/Bronze/20299. 3대 측정/3대 측정.py
@@ -7,11 +7,13 @@ cnt = 0
 ratings = []
 for _ in range(N):
     xs = list(map(int, input().split()))
+    S = 0
     for x in xs:
+        S += x
         if x < L:
             break
     else:
-        if K <= sum(xs):
+        if K <= S:
             cnt += 1
             ratings += xs
 print(cnt)
