8605729b9985f52b276ce411d7b39747617547fa
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2022-10-10 23:21:34 +0900

    [Silver V] Title: Numbers are Easy, Time: 380 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11322. Numbers are Easy/Numbers are Easy.py
+++ 백준/Silver/11322. Numbers are Easy/Numbers are Easy.py
@@ -1,7 +1,7 @@
 T = int(input())
 for _ in range(T):
     N = int(input())
-    for X in range(1, 10000000):
+    for X in range(1, 1000000):
         binary = int(bin(X)[2:])
         if binary % N == 0:
             print(binary)
