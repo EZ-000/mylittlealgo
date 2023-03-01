48507a51817854410c91046b61dc865eafb7b95e
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2023-03-01 21:04:20 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 5.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
 
 
-def rotate(pre_key):
+def rotate(pre_key):                                        # 시계방향으로 90도 회전하는 함수
     L = len(pre_key)
     rot_key = [[] for _ in range(L)]
     for i in range(L):
@@ -10,8 +10,8 @@ def rotate(pre_key):
     return rot_key
 
 
-def check(row, col, now_key, lock_copy):
-    for i in range(M):
+def check(row, col, now_key, lock_copy):                    # params: 현재 시작 위치, 현재 열쇠의 모양, 확장된 자물쇠의 복사본
+    for i in range(M):                                      # return: boolean 자물쇠가 열리는지 여부             
         for j in range(M):
             lock_copy[i + row][j + col] += now_key[i][j]
 
@@ -19,32 +19,29 @@ def check(row, col, now_key, lock_copy):
     for i in range(N):
         for j in range(N):
             now = lock_copy[M - 1 + i][M - 1 + j]
-            if now != 1:
+            if now != 1:                                    # 모두 1인 경우만 True
                 return False
     return True
     
     
 def solution(key, lock):
     global N
-    global M 
+    global M
     global L
-    
+
     N = len(lock)
     M = len(key)
-    add = [1] * (M - 1)
-    exp_lock = [add + l + add for l in lock]
     
-    add_lst = [1] * (2 * M + N - 2)
-    exp_lock = [add_lst] * (M - 1) + exp_lock + [add_lst] * (M - 1)
+    add_in = [1] * (M - 1)                                      # 자물쇠 확장
+    add_out = [1] * (2 * M + N - 2)
+    exp_lock = [add_out] * (M - 1) + [add_in + l + add_in for l in lock] + [add_out] * (M - 1)
     L = len(exp_lock)
     
-    answer = True
-    for _ in range(4):
+    for _ in range(4):                                          # 360도 돌면서 확인
         key = rotate(key)
         for i in range(N + M - 1):
             for j in range(N + M - 1):
                 lcopy = deepcopy(exp_lock)
                 if check(i, j, key, lcopy):
-                    return answer
-    answer = False            
-    return answer
+                    return True           
+    return False
