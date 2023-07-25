e39f2b19393cac9f85a47b285ede696b27545654
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2023-07-25 10:20:36 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=java) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 2.28 ms
+메모리: 73.6 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,14 +1,14 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
+        char[] chars = s.toCharArray();
         int index = 0;
-        String[] chars = s.split("");
-        
-        for (String c : chars) {
-            index = c.equals(" ") ? 0 : index + 1;
-            answer += (index % 2 == 0) ? c.toLowerCase() : c.toUpperCase();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ') index = 0;
+            else chars[i] = (index++ % 2 == 0)
+                    ? Character.toUpperCase(chars[i])
+                    : Character.toLowerCase(chars[i]);
         }
-        
-        return answer;
+        return String.valueOf(chars);
     }
 }
