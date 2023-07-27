5e1401ecc078c314798e4144192ffd5ee2dca921
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2023-07-27 17:34:45 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.22 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String myString) {
+        String[] items = myString.split(" ");
+        
+        int answer = Integer.parseInt(items[0]);
+        System.out.println(Arrays.toString(items));
+        for (int index = 1; index < items.length; index += 2) {
+            String op = items[index];
+            switch (op) {
+                case "+":
+                    answer += Integer.parseInt(items[index + 1]);
+                    break;
+                case "-":
+                    answer -= Integer.parseInt(items[index + 1]);
+                    break;
+            }
+        }
+        return answer;
+    }
+}
