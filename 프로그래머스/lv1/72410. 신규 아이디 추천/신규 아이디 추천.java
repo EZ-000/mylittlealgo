6d6e7cc61bff259149a6ee56c074ffb1a30f75ad
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2023-08-07 17:29:50 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.39 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,17 +1,65 @@
 class Solution {
     public String solution(String newId) {
-        newId = newId.toLowerCase();
-        newId = newId.replaceAll("[^a-z0-9\\-_.]","");
-        newId = newId.replaceAll("\\.+",".");
-        newId = newId.replaceAll("^\\.+|\\.+$","");
-        if (newId.isEmpty()) newId = "a";
-        if (16 <= newId.length()) {
-            newId = newId.substring(0, 15);
-            newId = newId.replaceAll("\\.+$","");
+        return new KakaoId(newId)
+                .replaceToLowerCase()
+                .filter()
+                .toSingleDot()
+                .noStartEndDot()
+                .noBlank()
+                .noGreaterThan16()
+                .noLessThan2()
+                .getResult();
+    }
+
+    private static class KakaoId {
+        private String s;
+
+        KakaoId(String s) {
+            this.s = s;
         }
-        while (newId.length() < 3) {
-            newId += newId.charAt(newId.length() - 1);
+
+        private KakaoId replaceToLowerCase() {
+            s = s.toLowerCase();
+            return this;
         }
-        return newId;
+
+        private KakaoId filter() {
+            s = s.replaceAll("[^a-z0-9._-]","");
+            return this;
+        }
+        
+        private KakaoId toSingleDot() {
+            s = s.replaceAll("[.]{2,}",".");
+            return this;
+        }
+        
+        private KakaoId noStartEndDot() {
+            s = s.replaceAll("^\\.|\\.$","");
+            return this;
+        }
+        
+        private KakaoId noBlank() {
+            s = s.isEmpty() ? "a" : s;
+            return this;
+        }
+        
+        private KakaoId noGreaterThan16() {
+            if (16 <= s.length()) {
+                s = s.substring(0, 15);
+                s = s.replaceAll("[.]$","");
+            }
+            return this;
+        }
+        
+        private KakaoId noLessThan2() {
+            while (s.length() < 3) {
+                s += s.charAt(s.length() - 1);
+            }
+            return this;
+        }
+        
+        private String getResult() {
+            return s;
+        }
     }
 }
