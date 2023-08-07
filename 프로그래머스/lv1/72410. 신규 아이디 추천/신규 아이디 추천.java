2d271f16dd0664682c93cf778ab8ea941dff926c
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2023-08-07 16:59:05 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.45 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,22 +1,17 @@
 class Solution {
-    public String solution(String new_id) {
-        // 1단계
-        new_id = new_id.toLowerCase();
-        // 2단계
-        new_id = new_id.replaceAll("[^a-z0-9\\-_.]","");
-        // 3단계
-        new_id = new_id.replaceAll("\\.{2,}",".");
-        // 4단계
-        new_id = new_id.replaceAll("^\\.|\\.$","");
-        // 5단계
-        new_id = (new_id.isEmpty()) ? "a" : new_id;
-        // 6단계
-        new_id = (16 <= new_id.length()) ? new_id.substring(0, 15) : new_id;
-        new_id = new_id.replaceAll("\\.$","");
-        // 7단계
-        int length = new_id.length();
-        String last = String.valueOf(new_id.charAt(length - 1));
-        new_id = (length < 3) ? new_id + last.repeat(3 - length) : new_id;
-        return new_id;
+    public String solution(String newId) {
+        newId = newId.toLowerCase();
+        newId = newId.replaceAll("[^a-z0-9\\-_.]","");
+        newId = newId.replaceAll("\\.+",".");
+        newId = newId.replaceAll("^\\.+|\\.+$","");
+        if (newId.isEmpty()) newId = "a";
+        if (16 <= newId.length()) {
+            newId = newId.substring(0, 15);
+            newId = newId.replaceAll("\\.+$","");
+        }
+        while (newId.length() < 3) {
+            newId += newId.charAt(newId.length() - 1);
+        }
+        return newId;
     }
 }
