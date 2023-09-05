ae9b7fdd2374cce542af30dea075d5d4efe51731
EZ-000 <85544352+EZ-000@users.noreply.github.com>
2023-09-05 15:17:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 300.41 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> counts = new HashMap<String, Integer>();
-
-        for (String person : participant) {
-            if (counts.containsKey(person)) counts.put(person, counts.get(person) + 1);
-            else counts.put(person, 0);
-        }
-
-        for (String person : completion) {
-            counts.put(person, counts.get(person) - 1);
-        }
-
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
         String answer = "";
-        for (String key : counts.keySet()) {
-            if (counts.get(key) == 0) {
-                answer = key;
+        for (int index = 0; index < completion.length; index++) {
+            if (!participant[index].equals(completion[index])) {
+                answer = participant[index];
                 break;
             }
         }
-
+        if (answer.equals("")) answer = participant[participant.length - 1];
+        
         return answer;
     }
 }
